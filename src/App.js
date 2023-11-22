ea07c52584cc9158f7ab532b32a0ac17e51e5f1a
Charitha Bandaraadhikaram <charitharuwindu@gmail.com>
2023-11-23 04:08:09 +0530

    only frontend remains and backend deleted

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="navbar_container">
+          <span className="navbtn">About</span>
+          <span className="navbtn">Education</span>
+          <span className="navbtn">Skills</span>
+          <span className="navbtn">Projects</span>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
