a01c52b0e7e42810e4f69344eed717b61382f2d6
Charitha Bandaraadhikaram <charitharuwindu@gmail.com>
2023-11-24 05:40:24 +0530

    added logo to title and added nav bar
    added underline effect to navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import React,{useState} from "react";
 import "./App.css";
 
 function App() {
+
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleNav = (index) =>{
+    setActiveTab(index);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="navbar_container">
-          <span className="navbtn">About</span>
-          <span className="navbtn">Education</span>
-          <span className="navbtn">Skills</span>
-          <span className="navbtn">Projects</span>
+          <ul>
+            <li className={`navbtn ${activeTab === 0 ? 'active' : ''}`} onClick={() => handleNav(0)}>About</li>
+            <li className={`navbtn ${activeTab === 1 ? 'active' : ''}`} onClick={() => handleNav(1)}>Education</li>
+            <li className={`navbtn ${activeTab === 2 ? 'active' : ''}`} onClick={() => handleNav(2)}>Skills</li>
+            <li className={`navbtn ${activeTab === 3 ? 'active' : ''}`} onClick={() => handleNav(3)}>Projects</li>
+          </ul>
         </div>
       </header>
     </div>
