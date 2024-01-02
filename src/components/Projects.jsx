40e5e49f3f6b9c870666b8e5ae38d72c622ee0e9
Charitha Bandaraadhikaram <charitharuwindu@gmail.com>
2024-01-02 08:58:54 +0530

    added borders and positioning

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,9 +10,11 @@ const Projects = forwardRef((props, ref) => {
         <div className="section_four" ref={ref}>
             <div className="project_details">
                 <h1>Projects</h1>
-                <Card style={{ width: '18rem' }}>
+                <div className="card_container">
+                <span className="card_holder">
+                <Card className="card" style={{ width: '18rem' }}>
                     <Card.Img variant="top" src={edugo2} className="proj_img"/>
-                    <Card.Body>
+                    <Card.Body className="c_body">
                         <Card.Title>Card Title</Card.Title>
                         <Card.Text>
                             Some quick example text to build on the card title and make up the
@@ -21,6 +23,22 @@ const Projects = forwardRef((props, ref) => {
                         <Button variant="primary">Go somewhere</Button>
                     </Card.Body>
                 </Card>
+                </span>
+
+                <span className="card_holder">
+                <Card className="card" style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={edugo2} className="proj_img"/>
+                    <Card.Body className="c_body">
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+                </span>
+                </div>
             </div>
         </div>
     );
