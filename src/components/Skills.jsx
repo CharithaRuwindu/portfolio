b1c1be31a67e2718dcfc0c88b46f492946c36001
Charitha Bandaraadhikaram <charitharuwindu@gmail.com>
2024-01-07 23:12:04 +0530

    started the skills page changes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,11 @@
 import React, {forwardRef} from "react";
 import "./css/skills.css";
+import html3logo from '../assets/html.png';
+import css5logo from '../assets/css.png';
+import jslogo from '../assets/js.png';
+import phplogo from '../assets/php.png';
+import mysqllogo from '../assets/mysql.png';
+import reactlogo from '../assets/react.png';
 
 const Skills = forwardRef((props, ref) => {
 
@@ -7,6 +13,12 @@ const Skills = forwardRef((props, ref) => {
         <div className="section_three" ref={ref}>
             <div className="skill_details">
           <h1>Skills</h1>
+        </div>
+        <div className="skill_container">
+            <div className="skill_logo"><img src={html3logo} alt="html logo" /></div>
+            <div className="skill_logo"><img src={css5logo} alt="css logo" /></div>
+            <div className="skill_logo"><img src={reactlogo} alt="react logo" /></div>
+            <div className="skill_logo"><img src={reactlogo} alt="react logo" /></div>
         </div>
         </div>
     );
