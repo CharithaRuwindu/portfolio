a92285ea58bda2bbcbc3441def8929413b4044a7
Charitha Bandaraadhikaram <charitharuwindu@gmail.com>
2024-01-13 23:58:38 +0530

    fixed few bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -94,10 +94,22 @@ function Navbar() {
         </div>
         <div className="menu_content">
           <ul>
-            <li>About</li>
-            <li>Education</li>
-            <li>Skills</li>
-            <li>Projects</li>
+            <li
+            className={`navbtn ${activeTab === 0 ? "active" : ""}`}
+            onClick={() => { handleNav(0); aboutRef.current.scrollIntoView({ behavior: 'smooth' }); }}>
+              About</li>
+            <li
+            className={`navbtn ${activeTab === 1 ? "active" : ""}`}
+            onClick={() => { handleNav(1); educationRef.current.scrollIntoView({ behavior: 'smooth' }); }}>
+              Education</li>
+            <li
+            className={`navbtn ${activeTab === 2 ? "active" : ""}`}
+            onClick={() => { handleNav(2); skillsRef.current.scrollIntoView({ behavior: 'smooth' }); }}>
+              Skills</li>
+            <li
+            className={`navbtn ${activeTab === 3 ? "active" : ""}`}
+            onClick={() => { handleNav(3); projectsRef.current.scrollIntoView({ behavior: 'smooth' }); }}>
+              Projects</li>
           </ul>
         </div>
         </div>
